49c1d0c9c0e197f82a2d8d4dcce0912d9da2c829
Johan Carlsson <johan@balowie.se>
2023-07-04 11:21:15 +0200

    Add settings link

--- simple-site-password-protector.php
+++ simple-site-password-protector.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Simple Site Password Protector
- * Version:1.0.1
+ * Version:1.0.2
  * Author: Johan Carlsson | Balowie
  * Description: Protect your site with a password
  * URL: https://www.balowie.com
@@ -30,3 +30,12 @@ require_once plugin_dir_path(__FILE__) . 'src/admin.php';
 if($status){
     require_once plugin_dir_path(__FILE__) . 'src/init.php';
 }
+
+function add_plugin_link( $plugin_actions, $plugin_file ) {
+    $new_actions = array();
+    if ( basename( plugin_dir_path( __FILE__ ) ) . '/simple-site-password-protector.php' === $plugin_file ) {
+        $new_actions['cl_settings'] = sprintf( __( '<a href="%s">Settings</a>', 'comment-limiter' ), esc_url( admin_url( 'tools.php?page=simple-site-password-protector' ) ) );
+    }
+    return array_merge( $new_actions, $plugin_actions );
+}
+add_filter( 'plugin_action_links', 'add_plugin_link', 10, 2 );
