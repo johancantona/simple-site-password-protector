ff8cba8e2aca4bef1649679b7a86823e8fa4163d
Johan Carlsson <johan@balowie.se>
2023-07-04 09:58:50 +0200

    Add version 1.0.1

--- simple-site-pwd-protector.php
+++ simple-site-pwd-protector.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Simple Site Password Protector
- * Version:1.0
+ * Version:1.0.1
  * Author: Johan Carlsson | Balowie
  * Description: Protect your site with a password
  * URL: https://www.balowie.com
