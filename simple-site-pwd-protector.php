cf3a03f49fb5e4d163a75dcea86d64db9ef0473f
Johan Carlsson <johan@balowie.se>
2023-07-04 09:40:01 +0200

    add plugin updater

--- simple-site-pwd-protector.php
+++ simple-site-pwd-protector.php
@@ -12,6 +12,16 @@ if (!defined('ABSPATH')) {
     exit();
 }
 
+require plugin_dir_path(__FILE__) . 'plugin-update-checker/plugin-update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://balowie.se/balowie-produkter/simple-site-pwd-protector/details.json',
+	__FILE__, //Full path to the main plugin file or functions.php.
+	'simple-site-pwd-protector'
+);
+
+
 global $status;
 $status = get_option('simple_site_pwd_protector_status') ? get_option('simple_site_pwd_protector_status') : false;
 
